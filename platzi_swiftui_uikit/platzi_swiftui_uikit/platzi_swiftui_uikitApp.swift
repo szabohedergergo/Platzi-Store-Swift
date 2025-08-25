eddc2acb276999551bcbf315282b21acc616b84d
Szabó Gergő <>
2025-08-25 13:06:46 +0200

    defa

---
+++ platzi_swiftui_uikit/platzi_swiftui_uikit/platzi_swiftui_uikitApp.swift
@@ -0,0 +1,17 @@
+//
+//  platzi_swiftui_uikitApp.swift
+//  platzi_swiftui_uikit
+//
+//  Created by Szabó Gergő on 2025. 08. 25..
+//
+
+import SwiftUI
+
+@main
+struct platzi_swiftui_uikitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ platzi_swiftui_uikit/platzi_swiftui_uikitTests/platzi_swiftui_uikitTests.swift
@@ -0,0 +1,17 @@
+//
+//  platzi_swiftui_uikitTests.swift
+//  platzi_swiftui_uikitTests
+//
+//  Created by Szabó Gergő on 2025. 08. 25..
+//
+
+import Testing
+@testable import platzi_swiftui_uikit
+
+struct platzi_swiftui_uikitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
